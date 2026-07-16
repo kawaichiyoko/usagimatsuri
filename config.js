2275f3448fc724f961a79dbfac7442ec86c0c5ae
kawaichiyoko <eichi3902@gmail.com>
2026-07-17 05:49:42 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.SHIORI_CONFIG = {
-  supabaseUrl: 'PASTE_YOUR_SUPABASE_PROJECT_URL_HERE',
-  supabaseAnonKey: 'PASTE_YOUR_SUPABASE_ANON_KEY_HERE',
+  supabaseUrl: 'https://whodklwwaowrfbqurpxl.supabase.co/rest/v1/',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indob2RrbHd3YW93cmZicXVycHhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQwNjQ3NjEsImV4cCI6MjA5OTY0MDc2MX0.WEga7XzhnVJnLHcisr-aVTUr38QvyQIUVk-z9vIlT24',
   tripId: 'kamakura-2026-7h4m9p2x'
 };
