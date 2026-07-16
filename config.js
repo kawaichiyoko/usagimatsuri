85a2f8fc33a994ca1c1392e359929a7e90f20456
kawaichiyoko <eichi3902@gmail.com>
2026-07-17 04:59:19 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+window.SHIORI_CONFIG = {
+  supabaseUrl: 'PASTE_YOUR_SUPABASE_PROJECT_URL_HERE',
+  supabaseAnonKey: 'PASTE_YOUR_SUPABASE_ANON_KEY_HERE',
+  tripId: 'kamakura-2026-7h4m9p2x',
+  // このしおりを管理するあなた自身のメールアドレス
+  ownerEmail: 'PASTE_YOUR_EMAIL_HERE'
+};
